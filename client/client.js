cf4ac87342de83705fe9b940026ba406c1f61140
Sam VB <you@example.com>
2014-01-20 20:03:18 -0500

    adding about section

--- client/client.js
+++ client/client.js
@@ -1,5 +1,5 @@
 Handlebars.registerHelper('info-box', function(name){
-	var html = "<div class='col-sm-4'><div class='col-sm-12 info-box'><h3>"+name+"</h3></div></div>";
+	var html = "<div class='col-sm-4'><div class='info-box'><h3>"+name+"</h3></div></div>";
 
 	return new Handlebars.SafeString(html);
 });
